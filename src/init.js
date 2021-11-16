2b5fc66affff886a6c629fb7abbf482bbd7b02b6
Kavita Poduri <kavi.pdri@gmail.com>
2021-11-15 20:27:11 -0800

    more experimentation

--- src/init.js
+++ src/init.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
+  // $('.addDancerButton').click(function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
@@ -15,7 +16,9 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('blinkyDancer');
+    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+    console.log("logged", makeDancer);
+    console.log(dancerMakerFunctionName);
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
